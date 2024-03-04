7a08e88b19a94da30d102d7e47e4b63054f3e8ff
johannalohmus <johanna.lohmus@gmail.com>
2024-03-03 19:18:49 -0500

    add and obfuscate email

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var lastUpdated = document.lastModified;
     document.getElementById('lastUpdated').textContent = lastUpdated;
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    var user = 'jklohmus';
+    var domain = 'unc.edu';
+    var element = document.getElementById('emailPlaceholder');
+    element.innerHTML = '<a href="mailto:' + user + '@' + domain + '">Contact Me</a>';
+});
